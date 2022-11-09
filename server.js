23ffe6474f37e2ca75082c8e6e8eb802ad3388f9
Emily Miles <ms.emily.j.miles@gmail.com>
2022-11-08 20:32:26 -0500

    Add base for express server

---
+++ server.js
@@ -0,0 +1,512 @@
+[
+  {
+    id: 1,
+    image: '',
+    name: 'Nova',
+    type: 'Feline',
+    age: 1,
+    DOB: '3/25/2021',
+    breed: 'Domestic Medium Hair',
+    color: 'Tortoiseshell Calico',
+    medHx: {
+      dxList: [
+        {
+          dxDate: '',
+          condition: '',
+          links: '',
+        }
+      ],
+      vxs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      labs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      exams: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      other: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        }
+      ],
+    },
+    appts: [],
+    meds: [],
+    alerts: [],
+    notes: [],
+  },
+  {
+    id: 2,
+    image: '',
+    name: 'Nebula',
+    type: 'Feline',
+    age: 1,
+    DOB: '7/12/2021',
+    breed: 'Domestic Medium Hair',
+    color: 'Dilute Tortoiseshell Calico',
+    medHx: {
+      dxList: [
+        {
+          dxDate: '',
+          condition: '',
+          links: '',
+        },
+      ],
+      vxs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      labs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      exams: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      other: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+    },
+    appts: [],
+    meds: [],
+    alerts: [],
+    notes: [],
+  },
+  {
+    id: 3,
+    image: '',
+    name: 'Diesel',
+    type: 'Canine',
+    age: 10,
+    DOB: 'need dob',
+    breed: 'Pit Bull Mix',
+    color: 'Tan and White',
+    medHx: {
+      dxList: [
+        {
+          dxDate: '',
+          condition: '',
+          links: '',
+        },
+      ],
+      vxs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      labs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      exams: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      other: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+    },
+    appts: [],
+    meds: [],
+    alerts: [],
+    notes: [],
+  },
+  {
+    id: 4,
+    image: '',
+    name: 'Penny',
+    type: 'Canine',
+    age: 7,
+    DOB: 'need dob',
+    breed: 'Duck Tolling Retriever Mix',
+    color: 'Copper',
+    medHx: {
+      dxList: [
+        {
+          dxDate: '',
+          condition: '',
+          links: '',
+        },
+      ],
+      vxs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      labs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      exams: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      other: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+    },
+    appts: [],
+    meds: [],
+    alerts: [],
+    notes: [],
+  },
+  {
+    id: 5,
+    image: '',
+    name: 'Beto',
+    type: 'Canine',
+    age: 2,
+    DOB: 'need dob',
+    breed: 'Pit Bull Mix',
+    color: 'Tan and White',
+    medHx: {
+      dxList: [
+        {
+          dxDate: '',
+          condition: '',
+          links: '',
+        }
+      ],
+      vxs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      labs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      exams: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      other: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        }
+      ],
+    },
+    appts: [],
+    meds: [],
+    alerts: [],
+    notes: [],
+  },
+  {
+    id: 6,
+    image: '',
+    name: 'Kesha',
+    type: 'Feline',
+    age: 'need age',
+    DOB: 'need dob',
+    breed: 'Domestic Short Hair',
+    color: 'Black',
+    medHx: {
+      dxList: [
+        {
+          dxDate: '',
+          condition: '',
+          links: '',
+        },
+      ],
+      vxs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      labs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      exams: [
+        {
+          received: '',
+          dueNext: '',
+          type: ''
+        }
+      ],
+      other: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+    },
+    appts: [],
+    meds: [],
+    alerts: [],
+    notes: [],
+  },
+  {
+    id: 7,
+    image: '',
+    name: 'Tyson',
+    type: 'Feline',
+    age: 'need dob',
+    DOB: 'need dob',
+    breed: 'Domestic Medium Hair',
+    color: 'Tuxedo',
+    medHx: {
+      dxList: [
+        {
+          dxDate: '',
+          condition: '',
+          links: '',
+        },
+      ],
+      vxs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      labs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      exams: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      other: [
+        {
+          received: '',
+          dueNext: '',
+          type: ''
+        },
+      ],
+    },
+    appts: [],
+    meds: [],
+    alerts: [],
+    notes: [],
+  },
+  {
+    id: 8,
+    image: '',
+    name: '',
+    type: '',
+    age: '',
+    DOB: '',
+    breed: '',
+    color: '',
+    medHx: {
+      dxList: [
+        {
+          dxDate: '',
+          condition: '',
+          links: '',
+        },
+      ],
+      vxs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      labs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      exams: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      other: [
+        {
+          received: '',
+          dueNext: '',
+          type: ''
+        },
+      ],
+    },
+    appts: [],
+    meds: [],
+    alerts: [],
+    notes: [],
+  },
+  {
+    id: 9,
+    image: '',
+    name: '',
+    type: '',
+    age: '',
+    DOB: '',
+    breed: '',
+    color: '',
+    medHx: {
+      dxList: [
+        {
+          dxDate: '',
+          condition: '',
+          links: '',
+        },
+      ],
+      vxs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      labs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      exams: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      other: [
+        {
+          received: '',
+          dueNext: '',
+          type: ''
+        },
+      ],
+    },
+    appts: [],
+    meds: [],
+    alerts: [],
+    notes: [],
+  },
+  {
+    id: 10,
+    image: '',
+    name: '',
+    type: '',
+    age: '',
+    DOB: '',
+    breed: '',
+    color: '',
+    medHx: {
+      dxList: [
+        {
+          dxDate: '',
+          condition: '',
+          links: '',
+        },
+      ],
+      vxs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      labs: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      exams: [
+        {
+          received: '',
+          dueNext: '',
+          type: '',
+        },
+      ],
+      other: [
+        {
+          received: '',
+          dueNext: '',
+          type: ''
+        },
+      ],
+    },
+    appts: [],
+    meds: [],
+    alerts: [],
+    notes: [],
+  },
+];
