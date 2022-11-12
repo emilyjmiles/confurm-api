8700ad95233ed96666afda4456e61be25657a5ea
Emily Miles <ms.emily.j.miles@gmail.com>
2022-11-12 12:35:08 -0500

    Refactor to organize data

---
+++ backup.js
@@ -0,0 +1,435 @@
+// {
+//   userId: 2,
+//   name: 'Debbie Aiken',
+//   address: '987 Mabon Lane, Sandy Hook, CT, 01234',
+//   email: 'cantstopwontstop@crazyplantlady.com',
+//   phone: '(987)312-0465',
+//   pets: [
+//     {
+//       petId: 3,
+//       image: '',
+//       name: 'Diesel',
+//       type: 'Canine',
+//       age: 10,
+//       DOB: 'need dob',
+//       breed: 'Pit Bull Mix',
+//       color: 'Tan and White',
+//       medHx: {
+//         dxList: [
+//           {
+//             dxDate: '',
+//             condition: '',
+//             links: '',
+//           },
+//         ],
+//         vxs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         labs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         exams: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         other: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//       },
+//       appts: [],
+//       meds: [],
+//       alerts: [],
+//       notes: [],
+//     },
+//     {
+//       petId: 4,
+//       image: '',
+//       name: 'Penny',
+//       type: 'Canine',
+//       age: 7,
+//       DOB: 'need dob',
+//       breed: 'Duck Tolling Retriever Mix',
+//       color: 'Copper',
+//       medHx: {
+//         dxList: [
+//           {
+//             dxDate: '',
+//             condition: '',
+//             links: '',
+//           },
+//         ],
+//         vxs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         labs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         exams: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         other: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//       },
+//       appts: [],
+//       meds: [],
+//       alerts: [],
+//       notes: [],
+//     },
+//   ]
+// },
+// {
+//   userId: 3,
+//   name: 'Robert Andrew',
+//   address: '456 Park Avenue, Aurora, C0, 65490',
+//   email: 'ibuildthings@musicislife.com',
+//   phone: '(201)975-3468',
+//   pets: [
+//     {
+//       id: 5,
+//       image: '',
+//       name: 'Beto',
+//       type: 'Canine',
+//       age: 2,
+//       DOB: 'need dob',
+//       breed: 'Pit Bull Mix',
+//       color: 'Tan and White',
+//       medHx: {
+//         dxList: [
+//           {
+//             dxDate: '',
+//             condition: '',
+//             links: '',
+//           }
+//         ],
+//         vxs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         labs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         exams: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         other: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           }
+//         ],
+//       },
+//       appts: [],
+//       meds: [],
+//       alerts: [],
+//       notes: [],
+//     },
+//   ]
+// },
+// {
+//   userId: 4,
+//   name: 'Kathleen Lent',
+//   address: '258 Sister Blvd, Sandy Hook, CT, 01234',
+//   email: 'meowmeow@crazyplantlady.com',
+//   phone: '',
+//   pets: [
+//     {
+//       id: 6,
+//       image: '',
+//       name: 'Kesha',
+//       type: 'Feline',
+//       age: 'need age',
+//       DOB: 'need dob',
+//       breed: 'Domestic Short Hair',
+//       color: 'Black',
+//       medHx: {
+//         dxList: [
+//           {
+//             dxDate: '',
+//             condition: '',
+//             links: '',
+//           },
+//         ],
+//         vxs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         labs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         exams: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: ''
+//           }
+//         ],
+//         other: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//       },
+//       appts: [],
+//       meds: [],
+//       alerts: [],
+//       notes: [],
+//     },
+//     {
+//       id: 7,
+//       image: '',
+//       name: 'Tyson',
+//       type: 'Feline',
+//       age: 'need dob',
+//       DOB: 'need dob',
+//       breed: 'Domestic Medium Hair',
+//       color: 'Tuxedo',
+//       medHx: {
+//         dxList: [
+//           {
+//             dxDate: '',
+//             condition: '',
+//             links: '',
+//           },
+//         ],
+//         vxs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         labs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         exams: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//         other: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: ''
+//           },
+//         ],
+//       },
+//       appts: [],
+//       meds: [],
+//       alerts: [],
+//       notes: [],
+//     },
+//   ]
+// }
+// {
+//   id: 8,
+//     image: '',
+//       name: '',
+//         type: '',
+//           age: '',
+//             DOB: '',
+//               breed: '',
+//                 color: '',
+//                   medHx: {
+//     dxList: [
+//       {
+//         dxDate: '',
+//         condition: '',
+//         links: '',
+//       },
+//     ],
+//       vxs: [
+//         {
+//           received: '',
+//           dueNext: '',
+//           type: '',
+//         },
+//       ],
+//         labs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//           exams: [
+//             {
+//               received: '',
+//               dueNext: '',
+//               type: '',
+//             },
+//           ],
+//             other: [
+//               {
+//                 received: '',
+//                 dueNext: '',
+//                 type: ''
+//               },
+//             ],
+//       },
+//   appts: [],
+//     meds: [],
+//       alerts: [],
+//         notes: [],
+//     },
+// {
+//   id: 9,
+//     image: '',
+//       name: '',
+//         type: '',
+//           age: '',
+//             DOB: '',
+//               breed: '',
+//                 color: '',
+//                   medHx: {
+//     dxList: [
+//       {
+//         dxDate: '',
+//         condition: '',
+//         links: '',
+//       },
+//     ],
+//       vxs: [
+//         {
+//           received: '',
+//           dueNext: '',
+//           type: '',
+//         },
+//       ],
+//         labs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//           exams: [
+//             {
+//               received: '',
+//               dueNext: '',
+//               type: '',
+//             },
+//           ],
+//             other: [
+//               {
+//                 received: '',
+//                 dueNext: '',
+//                 type: ''
+//               },
+//             ],
+//       },
+//   appts: [],
+//     meds: [],
+//       alerts: [],
+//         notes: [],
+//     },
+// {
+//   id: 10,
+//     image: '',
+//       name: '',
+//         type: '',
+//           age: '',
+//             DOB: '',
+//               breed: '',
+//                 color: '',
+//                   medHx: {
+//     dxList: [
+//       {
+//         dxDate: '',
+//         condition: '',
+//         links: '',
+//       },
+//     ],
+//       vxs: [
+//         {
+//           received: '',
+//           dueNext: '',
+//           type: '',
+//         },
+//       ],
+//         labs: [
+//           {
+//             received: '',
+//             dueNext: '',
+//             type: '',
+//           },
+//         ],
+//           exams: [
+//             {
+//               received: '',
+//               dueNext: '',
+//               type: '',
+//             },
+//           ],
+//             other: [
+//               {
+//                 received: '',
+//                 dueNext: '',
+//                 type: ''
+//               },
+//             ],
+//       },
+//   appts: [],
+//     meds: [],
+//       alerts: [],
+//         notes: [],
+//     },
