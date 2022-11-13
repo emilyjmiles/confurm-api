15e77314214735755aa1ba59096dda5654ac6fdf
Emily Miles <ms.emily.j.miles@gmail.com>
2022-11-13 14:13:51 -0500

    Fix data keys

--- server.js
+++ server.js
@@ -247,14 +247,14 @@ app.locals.apptRequests = [
   {
     id: 1,
     userName: 'Doug McNeil',
-    petName: 'Fluffy',
+    patientName: 'Fluffy',
     requestReason: 'Fluffy hasn\'t pooped in 3 days and started throwing up this morning',
     status: 'pending'
   },
   {
     id: 2,
     userName: 'Claire Boon',
-    petName: 'Bella',
+    patientName: 'Bella',
     requestReason: 'Bella is due for her annual exam',
     status: 'approved'
   }
